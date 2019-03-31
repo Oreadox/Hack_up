d4dd1572f101adb511e6e5f5275e5557b8d57361
yangchen29 <yangchen29@outlook.com>
2019-03-31 11:05:49 +0800

    建立房间模型

--- app/model/web_models.py
+++ app/model/web_models.py
@@ -4,6 +4,7 @@ from ..config import FlaskConfig
 from passlib.apps import custom_app_context
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired, BadSignature
+from datetime import datetime
 
 
 class User(db.Model):
@@ -12,9 +13,8 @@ class User(db.Model):
     username = db.Column(db.String(40), index=True, nullable=False)
     email = db.Column(db.String(40), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    confirmed = db.Column(db.Boolean, default=False)
-
-    # room_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    registration_time = db.Column(db.DateTime, default=datetime.now)
+    joined_room = db.Column(db.Boolean, default=False)
 
     def hash_password(self, password):
         self.password_hash = custom_app_context.encrypt(password)
@@ -38,5 +38,20 @@ class User(db.Model):
         user = User.query.get(data['id'])
         return user
 
-# class Room(db.Model):
-#     __tablename__ = 'rooms'
+
+class Room(db.Model):
+    __tablename__ = 'rooms'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    owner_id = db.Column(db.Integer, nullable=False)
+    count = db.Column(db.Integer, nullable=False, default=1)
+    create_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class Roommate(db.Model):
+    __tablename__ = 'roommates'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    join_time = db.Column(db.DateTime, default=datetime.now)
+    room = db.relationship('Room', backref='roommates', foreign_keys=room_id)
+    user = db.relationship('User', backref='roommates', foreign_keys=user_id)
