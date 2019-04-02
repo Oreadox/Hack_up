8fbfe1f9011755633f5f260cdf88cd0ee5a89611
yangchen29 <yangchen29@outlook.com>
2019-04-02 14:24:57 +0800

    添加聊天信息传输事件

--- app/api/web/socket.py
+++ app/api/web/socket.py
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-from app import socketio
 from flask_socketio import emit, Namespace, send, join_room
 from datetime import datetime
 from ...model.web_models import User
@@ -14,9 +13,13 @@ class RoomData(Namespace):
         time = str(datetime.now())
         emit('time', {'time': time})
 
-    def on_join_room(self, msg):
-        if msg.get('token'):
-            token = msg.get('token')[7:]
+    def on_connect(self):
+        send('连接成功')
+
+    def on_join_room(self, data):
+        token = ''
+        if data.get('token'):
+            token = data.get('token')[7:]
         else:
             emit('join_room', {'message': '需要token'})
         user = User.verify_auth_token(token)
@@ -26,3 +29,24 @@ class RoomData(Namespace):
             emit('join_room', {'message': '未加入房间'})
         room = user.roommember.room
         join_room(room='room_' + str(room.id))
+        join_room(room=user.id)
+        msg = []
+        for rm in room.roommembers:
+            dict = {}
+            dict['user_id'] = rm.user.id
+            ##还没写完
+            msg.append(dict)
+        emit('last_data', msg)
+
+    def on_message(self, data):
+        user_id = data.get('user_id')
+        message = data.get('message')
+        time = str(datetime.now())
+        user = User.query.filter_by(id=user_id).first()
+        msg = {
+            'user_id': user_id,
+            'message': message,
+            'time': time
+        }
+        room = user.roommember.room
+        emit('message', msg, room='room_' + str(room.id))
